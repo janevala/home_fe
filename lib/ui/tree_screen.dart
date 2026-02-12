2aa5e381ee23f50ca1f2fed9d79fada82b298630
J. Nevala <nevala.jarno@gmail.com>
2026-02-12 14:50:28 +0700

    tree view

--- lib/ui/tree_screen.dart
+++ lib/ui/tree_screen.dart
@@ -4,26 +4,23 @@ import 'package:go_router/go_router.dart';
 import 'package:homefe/bloc/rss_bloc.dart';
 import 'package:homefe/functions.dart';
 import 'package:homefe/podo/rss/news_item.dart';
-import 'package:homefe/ui/callback_shortcuts.dart';
 import 'package:homefe/ui/list_tile.dart';
 import 'package:homefe/ui/spinner.dart';
+import 'package:two_dimensional_scrollables/two_dimensional_scrollables.dart';
 
-class ArchiveScreen extends StatefulWidget {
-  const ArchiveScreen({super.key});
+class TreeScreen extends StatefulWidget {
+  const TreeScreen({super.key});
 
   @override
-  ArchiveScreenState createState() => ArchiveScreenState();
+  TreeScreenState createState() => TreeScreenState();
 }
 
-class ArchiveScreenState extends State<ArchiveScreen> {
-  final _scrollContoller = ScrollController();
+class TreeScreenState extends State<TreeScreen> {
   final _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-
-    _scrollContoller.addListener(_onScroll);
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<RssArchiveBloc>().add(ResetArchive());
@@ -33,18 +30,9 @@ class ArchiveScreenState extends State<ArchiveScreen> {
 
   @override
   void dispose() {
-    _scrollContoller.removeListener(_onScroll);
-    _scrollContoller.dispose();
     _searchController.dispose();
 
     super.dispose();
-  }
-
-  void _onScroll() {
-    if (_scrollContoller.position.pixels >=
-        (_scrollContoller.position.maxScrollExtent)) {
-      context.read<RssArchiveBloc>().add(LoadMoreArchive());
-    }
   }
 
   @override
@@ -97,16 +85,11 @@ class ArchiveScreenState extends State<ArchiveScreen> {
                 if (state is Loading) {
                   return Flexible(child: Center(child: const Spinner()));
                 } else if (state is ArchiveLoad) {
-                  return _buildList(context, state.items, _scrollContoller);
+                  return _buildTree(context, state.items);
                 } else if (state is ArchiveLoadMore) {
-                  return _buildList(
-                    context,
-                    state.items,
-                    _scrollContoller,
-                    isLoadingMore: true,
-                  );
+                  return _buildTree(context, state.items, isLoadingMore: true);
                 } else if (state is SearchLoad) {
-                  return _buildSearchList(context, state.items);
+                  return _buildSearchTree(context, state.items);
                 } else if (state is Failure) {
                   return Flexible(
                     child: Center(
@@ -143,44 +126,39 @@ class ArchiveScreenState extends State<ArchiveScreen> {
     );
   }
 
-  Widget _buildList(
+  Widget _buildTree(
     BuildContext context,
-    List<NewsItem> items,
-    ScrollController scroll, {
+    List<NewsItem> items, {
     bool isLoadingMore = false,
   }) {
     return Flexible(
-      child: CallbackShortcuts(
-        bindings: getCallbackShortcuts(scroll),
-        child: Focus(
-          autofocus: true,
-          child: ListView.builder(
-            controller: scroll,
-            itemCount: items.length + (isLoadingMore ? 1 : 0),
-            itemBuilder: (BuildContext context, int index) {
-              if (index >= items.length) {
-                return Center(child: CircularProgressIndicator());
-              }
+      child: Focus(
+        autofocus: true,
+        child: ListView.builder(
+          itemCount: items.length + (isLoadingMore ? 1 : 0),
+          itemBuilder: (BuildContext context, int index) {
+            if (index >= items.length) {
+              return Center(child: CircularProgressIndicator());
+            }
 
-              NewsItem item = items[index];
-              return JsonFeedTile(
-                key: Key(item.link),
-                onItemTap: () => openItem(context, item),
-                onItemLongPress: () {
-                  context.read<RssArchiveBloc>().add(
-                    QuestionEvent('translate to spanish: ${item.title}'),
-                  );
-                },
-                item: item,
-              );
-            },
-          ),
+            NewsItem item = items[index];
+            return JsonFeedTile(
+              key: Key(item.link),
+              onItemTap: () => openItem(context, item),
+              onItemLongPress: () {
+                context.read<RssArchiveBloc>().add(
+                  QuestionEvent('translate to spanish: ${item.title}'),
+                );
+              },
+              item: item,
+            );
+          },
         ),
       ),
     );
   }
 
-  Widget _buildSearchList(BuildContext context, List<NewsItem> items) {
+  Widget _buildSearchTree(BuildContext context, List<NewsItem> items) {
     return Flexible(
       child: Focus(
         autofocus: true,
